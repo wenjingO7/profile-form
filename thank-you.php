c67c39d77212b0e9160885652cb2e100b1fd8f81
wenjingO7 <42975417+wenjingO7@users.noreply.github.com>
2018-09-26 14:06:45 -0400

    add

---
+++ thank-you.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+	<head>
+		<title> thank-you.php</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<p> Thanks for your info, refresh this page. </p>
+	</body>
+</html>
